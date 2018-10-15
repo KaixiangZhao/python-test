5e1315cfdc4e1845c3d11cad1b59867804b64d04
Kaixiang Zhao <zkaixian@amazon.com>
2018-10-15 10:18:19 -0700

    update again

--- main.py
+++ main.py
@@ -1,2 +1,11 @@
-import boto3
-print("I'm here")
+import json
+import datetime
+
+def handler(event, context):
+    data = {
+        'output': 'Hello World',
+        'timestamp': datetime.datetime.utcnow().isoformat()
+    }
+    return {'statusCode': 200,
+            'body': json.dumps(data),
+            'headers': {'Content-Type': 'application/json'}}
